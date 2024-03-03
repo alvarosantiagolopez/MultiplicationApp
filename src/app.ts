4be6e19525c4882e02e7ca0c50ff02cae519f27a
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-03-03 07:54:12 +0100

    create table 5

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,21 @@
+import fs from 'fs';
 
-const message: string = 'Hola Mundo'
+let outputMessage = '';
+const base = 5;
+const headerMessage = `
+=================================
+        Tabla del ${base} 
+=================================\n
+`;
 
-console.log(message)
+for (let i = 1; i <= 10; i++) {
+    outputMessage += `${base} x ${i} = ${base * i}\n`
+}
+
+outputMessage = headerMessage + outputMessage;
+console.log(outputMessage)
+
+const outputPath = 'outputs'
+
+fs.mkdirSync(outputPath, { recursive: true })
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
