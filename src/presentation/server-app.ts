5f99d4ee9907c8faa141438053c6b9ea593ce91a
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-03-05 16:52:54 +0100

    save-file

--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,3 +1,4 @@
+import { SaveFile } from "../domain/save-file.use-case";
 import { CreateTable } from "../domain/use-cases/create-table.use-case";
 
 
@@ -15,8 +16,17 @@ export class ServerApp {
         console.log('Server running...')
 
         const table = new CreateTable().execute({ base, limit });
+        const wasCreated = new SaveFile()
+            .execute({
+                fileContent: table,
+                fileDestination: `outputs/table${base}`
+            });
 
-        if (showTable) console.log(table)
+        if (showTable) console.log(table);
+
+        (wasCreated)
+            ? console.log('File created!')
+            : console.log('File not created!');
     }
 
 }
